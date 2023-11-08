b5a5c14f8eee866ad77f6493bd490e43f59d402c
victoras24 <fin17217@uom.edu.gr>
2023-11-08 23:12:37 +0200

    mobile view fixed

--- components/Header.jsx
+++ components/Header.jsx
@@ -41,7 +41,7 @@ export default function Header() {
                         className="login-icon"
                     />
                 </Link>
-                <button onClick={fakeLogOut}>X</button>
+
             </nav>
         </header>
     )
